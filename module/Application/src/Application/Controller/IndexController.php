b9ec7664778e4d4262b636731d86ae06bb266d04
Mist02468 <mist@ptd.net>
2016-01-27 18:44:30 -0500

    Got The Collection of a Stack of YouTube Video Ids Working

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -26,12 +26,62 @@ class IndexController extends AbstractActionController
         $secrets = parse_ini_file('config/autoload/localSecrets.ini');
         $apiKey  = $secrets['YouTubePublicData'];
         
-        $request  = 'https://www.googleapis.com/youtube/v3/videos?id=' . $firstVideoId . '&key=' . $apiKey . '&part=snippet';
+        $request   = 'https://www.googleapis.com/youtube/v3/videos?id=' . $firstVideoId . '&key=' . $apiKey . '&part=snippet';
+        $response  = $this->makeCurlRequest($request);
+        $channelId = $response['items'][0]['snippet']['channelId'];
+        
+        $request    = 'https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=' . $channelId . '&key=' . $apiKey;
+        $response   = $this->makeCurlRequest($request);
+        $playlistId = $response['items'][0]['contentDetails']['relatedPlaylists']['uploads'];
+        
+        $videosStack   = new \SplStack();
+        $currentIndex  = 0;
+        $currentVideos = array();
+        $nextPageToken = 'first';
+        $successful    = True;
+        do {
+            if (array_key_exists($currentIndex, $currentVideos) === False) {
+                if ($nextPageToken == 'last') {
+                    $successful = False;
+                    break;
+                }
+                
+                $request       = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50' . ($nextPageToken != 'first' ? '&pageToken=' . $nextPageToken : '') . '&playlistId=' . $playlistId . '&key=' . $apiKey;
+                $response      = $this->makeCurlRequest($request);
+                
+                if (array_key_exists('nextPageToken', $response) === False) {
+                    $nextPageToken = 'last';
+                } else {
+                    $nextPageToken = $response['nextPageToken'];
+                }
+                $currentVideos = $response['items'];
+                $currentIndex  = 0;
+            }
+            
+            $currentVideoId = $currentVideos[$currentIndex]['snippet']['resourceId']['videoId'];
+            $videosStack[]  = $currentVideoId;
+            $currentIndex++;
+        } while ($currentVideoId !== $firstVideoId);
+        
+        if ($successful) {
+            var_dump("successful!");
+            var_dump($videosStack);
+        } else {
+            var_dump("NOT successful");
+        }
+    }
+    
+    private function makeCurlRequest($request) {
         $handle   = curl_init($request);
         curl_setopt($handle, CURLOPT_RETURNTRANSFER, True);
         $response = json_decode(curl_exec($handle), True);
+        $errors   = curl_error($handle);
         curl_close($handle);
         
-        $channelId = $response['items'][0]['snippet']['channelId'];
+        if (empty($errors)) {
+            return $response;
+        } else {
+            throw new Exception($errors);
+        }
     }
 }
